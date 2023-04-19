e4f7eeeb3714de202c5c53bd4005506f48cb7f70
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-04-19 16:06:56 +0600

    calculating saving

--- js/calculation.js
+++ js/calculation.js
@@ -31,3 +31,20 @@ const calculateExpense = () => {
     document.getElementById("balance").innerText = balance;
   }
 };
+
+const calculateSavings = () => {
+  const income = document.querySelector("#income").value;
+  // calculate saving amount
+  const savePercentage = document.getElementById("save").value;
+//   Validate saving percentage value
+  if (savePercentage < 0) {
+    alert("Provide positive saving value");
+    return;
+  }
+  console.log(income, savePercentage);
+  const savingAmount = (savePercentage / 100) * income;
+
+
+
+
+
